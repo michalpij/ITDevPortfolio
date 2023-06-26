afaa81adc6e50036c9ae504ad93b8c66bbcee1d3
michalpij <jaku0001@hz.nl>
2023-06-26 10:46:46 +0200

    Added routing to buttons from the welcome view to each challenge category

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -807,7 +807,7 @@
 
         <div class="mt-16">
             <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
-                <a href="https://laravel.com/docs"
+                <a href="/devops"
                    class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
                     <div>
                         <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">DevOps</h2>
@@ -829,7 +829,7 @@
                     </svg>
                 </a>
 
-                <a href="https://laracasts.com"
+                <a href="/usability"
                    class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
                     <div>
                         <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Usability</h2>
@@ -849,7 +849,7 @@
                     </svg>
                 </a>
 
-                <a href="https://laravel-news.com"
+                <a href="/innovation"
                    class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
                     <div>
                         <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Innovation</h2>
@@ -870,7 +870,7 @@
                     </svg>
                 </a>
 
-                <a href="https://laracasts.com"
+                <a href="/assessment"
                    class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
                     <div>
                         <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Project Assessment</h2>
